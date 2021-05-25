14f6d2d93ec5b167d0a14d02b7736150cbfea194
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2021-05-25 12:45:15 +0100

    PODS-5324 WYWN page

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -11,7 +11,7 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "play-language"                  % "5.0.0-play-27",
     "uk.gov.hmrc"                   %%  "play-nunjucks"                  % "0.27.0-play-27",
     "uk.gov.hmrc"                   %%  "play-nunjucks-viewmodel"        % "0.13.0-play-27",
-    "org.webjars.npm"               %   "govuk-frontend"                 % "3.5.0",
+    "org.webjars.npm"               %   "govuk-frontend"                 % "3.7.0",
     "org.webjars.npm"               %   "hmrc-frontend"                  % "1.19.0",
     "com.google.inject.extensions"  %   "guice-multibindings"            % "4.2.2",
     "uk.gov.hmrc"             %% "domain"                         % "5.11.0-play-27"
