e112b3afc40a0239a29d708fbe8217ffb6eaddc5
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2021-05-26 07:59:37 +0100

    PODS-5324 WIP

--- app/controllers/benefitsAndInsurance/IsInvestmentRegulatedController.scala
+++ app/controllers/benefitsAndInsurance/IsInvestmentRegulatedController.scala
@@ -21,15 +21,16 @@ import connectors.cache.UserAnswersCacheConnector
 import controllers.Retrievals
 import controllers.actions._
 import forms.benefitsAndInsurance.IsInvestmentRegulatedFormProvider
-import identifiers.beforeYouStart.SchemeNameId
+import identifiers.beforeYouStart.{SchemeNameId, SchemeTypeId}
+import identifiers.benefitsAndInsurance.IsInvestmentRegulatedId
 import navigators.CompoundNavigator
-import play.api.i18n.{MessagesApi, I18nSupport}
+import play.api.data.Form
+import play.api.i18n.{MessagesApi, Messages, I18nSupport}
 import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import renderer.Renderer
-import uk.gov.hmrc.viewmodels.Radios
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
-import uk.gov.hmrc.viewmodels.NunjucksSupport
+import uk.gov.hmrc.viewmodels.{Radios, NunjucksSupport}
 import utils.Enumerable
 
 import javax.inject.Inject
@@ -47,69 +48,53 @@ class IsInvestmentRegulatedController @Inject()(override val messagesApi: Messag
                                        renderer: Renderer)(implicit ec: ExecutionContext)
   extends FrontendBaseController  with I18nSupport with Retrievals with Enumerable.Implicits with NunjucksSupport {
 
-  private def form(memberName: String)(implicit messages: Messages): Form[Boolean] =
-    formProvider(messages("isInvestmentRegulated.error.required", memberName))
+  private def form(schemeName: String)(implicit messages: Messages): Form[Boolean] =
+    formProvider(messages("isInvestmentRegulated.error.required", schemeName))
 
   def onPageLoad: Action[AnyContent] =
     (authenticate andThen getData andThen requireData).async { implicit request =>
       SchemeNameId.retrieve.right.map { schemeName =>
+        val preparedForm = request.userAnswers.get(IsInvestmentRegulatedId) match {
+          case Some(value) => form(schemeName).fill(value)
+          case None        => form(schemeName)
+        }
         val json = Json.obj(
           "schemeName" -> schemeName,
-          "radios" -> Radios.yesNo(form(employerName)(implicitly)("value")),
+          "form" -> preparedForm,
+          //"radios" -> Radios.yesNo(form(schemeName)(implicitly)("value")),
+          "radios" -> Radios.yesNo (preparedForm("value")),
+          "submitUrl" -> controllers.benefitsAndInsurance.routes.IsInvestmentRegulatedController.onSubmit().url,
           "returnUrl" -> controllers.routes.TaskListController.onPageLoad().url
         )
-            renderer.render("benefitsAndInsurance/isInvestmentRegulated.njk", json).map(Ok(_))
-        }
+        renderer.render("benefitsAndInsurance/isInvestmentRegulated.njk", json).map(Ok(_))
       }
+    }
 
-//  def onSubmit(srn: String, startDate: LocalDate, accessType: AccessType, version: Int, index: Index): Action[AnyContent] =
-//    (identify andThen getData(srn, startDate) andThen requireData).async { implicit request =>
-//      DataRetrievals.retrieveSchemeName { schemeName =>
-//        request.userAnswers.get(MemberDetailsPage(index)) match {
-//          case Some(memberDetails) =>
-//            form(memberDetails.fullName)
-//              .bindFromRequest()
-//              .fold(
-//                formWithErrors => {
-//
-//                  val viewModel = GenericViewModel(
-//                    submitUrl = routes.DeleteMemberController.onSubmit(srn, startDate, accessType, version, index).url,
-//                    returnUrl = controllers.routes.ReturnToSchemeDetailsController.returnToSchemeDetails(srn, startDate, accessType, version).url,
-//                    schemeName = schemeName
-//                  )
-//
-//                  val json = Json.obj(
-//                    "srn" -> srn,
-//                    "startDate" -> Some(startDate),
-//                    "form" -> formWithErrors,
-//                    "viewModel" -> viewModel,
-//                    "radios" -> Radios.yesNo(formWithErrors("value")),
-//                    "memberName" -> memberDetails.fullName
-//                  )
-//
-//                  renderer.render("chargeD/deleteMember.njk", json).map(BadRequest(_))
-//
-//                },
-//                value =>
-//                  if (value) {
-//                    DataRetrievals.retrievePSTR {
-//                      pstr =>
-//                        for {
-//                          updatedAnswers <- Future.fromTry(userAnswersService
-//                            .removeMemberBasedCharge(MemberDetailsPage(index), totalAmount(srn, startDate, accessType, version)))
-//
-//                          _ <- deleteAFTChargeService.deleteAndFileAFTReturn(pstr, updatedAnswers)
-//                        } yield Redirect(navigator.nextPage(DeleteMemberPage, NormalMode, updatedAnswers, srn, startDate, accessType, version))
-//                    }
-//                  } else {
-//                    Future.successful(Redirect(navigator.nextPage(DeleteMemberPage, NormalMode, request.userAnswers, srn, startDate, accessType, version)))
-//                  }
-//              )
-//          case _ => Future.successful(Redirect(controllers.routes.SessionExpiredController.onPageLoad()))
-//        }
-//      }
-//    }
-//
-//  private def totalAmount(srn: String, startDate: LocalDate, accessType: AccessType, version: Int)(implicit request: DataRequest[AnyContent]): UserAnswers => BigDecimal =
-//    chargeDHelper.getLifetimeAllowanceMembers(_, srn, startDate, accessType, version).map(_.amount).sum
+  def onSubmit: Action[AnyContent] =
+    (authenticate andThen getData andThen requireData).async { implicit request =>
+      SchemeNameId.retrieve.right.map { schemeName =>
+        form(schemeName)
+          .bindFromRequest()
+          .fold(
+            formWithErrors => {
+              val json = Json.obj(
+                "schemeName" -> schemeName,
+                "form" -> formWithErrors,
+                "radios" -> Radios.yesNo(form(schemeName)(implicitly)("value")),
+                "submitUrl" -> controllers.benefitsAndInsurance.routes.IsInvestmentRegulatedController.onSubmit().url,
+                "returnUrl" -> controllers.routes.TaskListController.onPageLoad().url
+              )
+
+              renderer.render("benefitsAndInsurance/isInvestmentRegulated.njk", json).map(BadRequest(_))
+            },
+            value => {
+              val updatedUA = request.userAnswers.setOrException(IsInvestmentRegulatedId, value)
+              userAnswersCacheConnector.save(request.lock, updatedUA.data).map { _ =>
+                Redirect(navigator.nextPage(SchemeTypeId, updatedUA))
+              }
+            }
+          )
+        }
+    }
+
 }
