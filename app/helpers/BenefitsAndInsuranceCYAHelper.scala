602eee884f7b25b432c70afa0d97fa3eb5c61428
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2021-05-27 15:29:40 +0100

    PODS-5324 WIP

--- app/helpers/BenefitsAndInsuranceCYAHelper.scala
+++ app/helpers/BenefitsAndInsuranceCYAHelper.scala
@@ -17,38 +17,70 @@
 package helpers
 
 import identifiers.beforeYouStart.SchemeNameId
-import identifiers.benefitsAndInsurance.{AreBenefitsSecuredId, BenefitsInsurancePolicyId, BenefitsTypeId, HowProvideBenefitsId, InsurerAddressId, InsurerAddressListId, IsInvestmentRegulatedId, IsOccupationalId}
+import identifiers.benefitsAndInsurance.{AreBenefitsSecuredId, BenefitsInsurancePolicyId, InsurerAddressListId, BenefitsInsuranceNameId, InsurerAddressId, BenefitsTypeId, HowProvideBenefitsId, IsInvestmentRegulatedId, IsOccupationalId}
 import models.{Address, Members}
 import play.api.i18n.Messages
 import play.api.mvc.AnyContent
 import uk.gov.hmrc.viewmodels.{MessageInterpolators, SummaryList, Text}
-import utils.{Enumerable, UserAnswers}
+import utils.{UserAnswers, Enumerable}
 import viewmodels.Message
-import identifiers.aboutMembership.{CurrentMembersId, FutureMembersId}
+import identifiers.aboutMembership.{FutureMembersId, CurrentMembersId}
 import identifiers.beforeYouStart.SchemeNameId
 import models.Members
+import models.benefitsAndInsurance.BenefitsProvisionType.DefinedBenefitsOnly
 import models.benefitsAndInsurance.{BenefitsProvisionType, BenefitsType}
 import models.requests.DataRequest
 import play.api.i18n.Messages
 import play.api.mvc.AnyContent
 import uk.gov.hmrc.viewmodels.{MessageInterpolators, SummaryList, Text}
-import utils.{Enumerable, UserAnswers}
+import utils.{UserAnswers, Enumerable}
 import viewmodels.Message
 
 class BenefitsAndInsuranceCYAHelper extends CYAHelper with Enumerable.Implicits{
+
+  private val answerBooleanTransform: Option[Boolean => Text] = Some(opt => msg"booleanAnswer.${opt.toString}")
+  private val answerStringTransform: Option[String => Text] = Some(opt => lit"${opt}")
+  private val answerBenefitsProvisionTypeTransform: Option[BenefitsProvisionType => Text] = Some(opt => msg"howProvideBenefits.${opt.toString}")
+  private val answerBenefitsTypeTransform: Option[BenefitsType => Text] = Some(opt => msg"benefitsType.${opt.toString}")
+  private val answerBenefitsAddressTransform: Option[Address => Text] = Some(opt => lit"${opt.toString}")
+
+
   def rows(implicit request: DataRequest[AnyContent],
     messages: Messages
   ): Seq[SummaryList.Row] = {
     implicit val ua: UserAnswers = request.userAnswers
     val schemeName = getAnswer(SchemeNameId)
 
-    val answerBooleanTransform: Option[Boolean => Text] = Some(opt => msg"booleanAnswer.${opt.toString}")
-    val answerStringTransform: Option[String => Text] = Some(opt => lit"${opt}")
-    val answerBenefitsProvisionTypeTransform: Option[BenefitsProvisionType => Text] = Some(opt => msg"howProvideBenefits.${opt.toString}")
-    val answerBenefitsTypeTransform: Option[BenefitsType => Text] = Some(opt => msg"benefitsType.${opt.toString}")
-    val answerBenefitsAddressTransform: Option[Address => Text] = Some(opt => lit"${opt.toString}")
+    val seqTop = topSection(schemeName)
+    val seqBottom = if(ua.get(AreBenefitsSecuredId).contains(true))
+      Seq(
+        answerOrAddRow(
+        BenefitsInsuranceNameId,
+        Message("benefitsInsuranceName.title").resolve,
+        controllers.benefitsAndInsurance.routes.BenefitsInsuranceNameController.onPageLoad().url,
+        Some(msg"messages__visuallyhidden__currentMembers"), answerStringTransform
+        ),
+      answerOrAddRow(
+        BenefitsInsurancePolicyId,
+        Message("benefitsInsurancePolicy.title").resolve,
+        controllers.benefitsAndInsurance.routes.BenefitsInsurancePolicyController.onPageLoad().url,
+        Some(msg"messages__visuallyhidden__currentMembers"), answerStringTransform
+      ),
+      answerOrAddRow(
+        InsurerAddressId,
+        Message("addressFor", schemeName).resolve,
+        controllers.benefitsAndInsurance.routes.InsurerEnterPostcodeController.onPageLoad().url,
+        Some(msg"messages__visuallyhidden__currentMembers"), answerBenefitsAddressTransform
+      )
+    )
+    else
+      Nil
 
-    val seq = Seq(
+    seqTop ++ seqBottom
+  }
+
+  private def topSection(schemeName: String)(implicit messages: Messages, ua: UserAnswers) = {
+    val s1 = Seq(
       answerOrAddRow(
         IsInvestmentRegulatedId,
         Message("isInvestmentRegulated.h1", schemeName).resolve,
@@ -66,38 +98,25 @@ class BenefitsAndInsuranceCYAHelper extends CYAHelper with Enumerable.Implicits{
         Message("howProvideBenefits.title").resolve,
         controllers.benefitsAndInsurance.routes.HowProvideBenefitsController.onPageLoad().url,
         Some(msg"messages__visuallyhidden__currentMembers"), answerBenefitsProvisionTypeTransform
-      ),
+      )
+    )
+    val s2 = if(ua.get(HowProvideBenefitsId).contains(DefinedBenefitsOnly)) Nil else Seq(
       answerOrAddRow(
         BenefitsTypeId,
         Message("benefitsType.h1", schemeName).resolve,
         controllers.benefitsAndInsurance.routes.BenefitsTypeController.onPageLoad().url,
         Some(msg"messages__visuallyhidden__currentMembers"), answerBenefitsTypeTransform
-      ),
-        answerOrAddRow(
-          AreBenefitsSecuredId,
+      )
+    )
+
+    val s3 = Seq(
+      answerOrAddRow(
+        AreBenefitsSecuredId,
         Message("areBenefitsSecured.title").resolve,
         controllers.benefitsAndInsurance.routes.AreBenefitsSecuredController.onPageLoad().url,
         Some(msg"messages__visuallyhidden__currentMembers"), answerBooleanTransform
       )
     )
-    val seqTest = if(ua.get(AreBenefitsSecuredId).contains(true))Seq(
-
-      answerOrAddRow(
-        BenefitsInsurancePolicyId,
-        Message("benefitsInsurancePolicy.title").resolve,
-        controllers.benefitsAndInsurance.routes.BenefitsInsurancePolicyController.onPageLoad().url,
-        Some(msg"messages__visuallyhidden__currentMembers"), answerStringTransform
-      ),
-      answerOrAddRow(
-        InsurerAddressId,
-        Message("addressFor", schemeName).resolve,
-        controllers.benefitsAndInsurance.routes.InsurerEnterPostcodeController.onPageLoad().url,
-        Some(msg"messages__visuallyhidden__currentMembers"), answerBenefitsAddressTransform
-      )
-    )
-    else
-      Nil
-
-    seq ++ seqTest
+    s1 ++ s2 ++ s3
   }
 }
