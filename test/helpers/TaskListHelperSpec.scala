bcc9cebe0124636ac1e74570ee88661c94d31097
Madhura <madhuranakate@gmail.com>
2021-06-03 11:29:49 +0100

    PODS-5195 ADded few tests

--- test/helpers/TaskListHelperSpec.scala
+++ test/helpers/TaskListHelperSpec.scala
@@ -23,7 +23,7 @@ import org.mockito.Mockito.when
 import org.scalatest.{MustMatchers, WordSpec}
 import org.scalatestplus.mockito.MockitoSugar
 import play.api.i18n.Messages
-import utils.Data.{schemeName, ua}
+import utils.Data.{completeUserAnswers, schemeName, ua}
 import utils.UserAnswers
 import viewmodels.{Message, TaskListEntitySection}
 
@@ -32,10 +32,20 @@ class TaskListHelperSpec extends SpecBase with MustMatchers with MockitoSugar {
   private val mockSpokeCreationService = mock[SpokeCreationService]
   private val helper = new TaskListHelper(mockSpokeCreationService)
   private val beforeYouStartLinkText = messages("messages__schemeTaskList__before_you_start_link_text", schemeName)
+  private val membershipDetailsLinkText = messages("messages__schemeTaskList__about_members_link_text", schemeName)
+  private val declarationLinkText = messages("messages__schemeTaskList__declaration_link")
   private val beforeYouStartHeader = Some(Message("messages__schemeTaskList__before_you_start_header"))
+  private val aboutHeader = Some(Message("messages__schemeTaskList__about_scheme_header", schemeName))
+  private val declarationHeader = Some("messages__schemeTaskList__sectionDeclaration_header")
+  private val declarationP1 = List("messages__schemeTaskList__sectionDeclaration_incomplete_v1",
+    "messages__schemeTaskList__sectionDeclaration_incomplete_v2")
 
   private val expectedBeforeYouStartSpoke = Seq(EntitySpoke(TaskListLink(beforeYouStartLinkText,
     controllers.beforeYouStartSpoke.routes.CheckYourAnswersController.onPageLoad().url), Some(false)))
+  private val expectedMembershipDetailsSpoke = EntitySpoke(TaskListLink(membershipDetailsLinkText,
+    controllers.aboutMembership.routes.CheckYourAnswersController.onPageLoad().url), Some(false))
+  private val expectedDeclarationSpoke = EntitySpoke(TaskListLink(declarationLinkText,
+    controllers.routes.DeclarationController.onPageLoad().url), Some(false))
 
   implicit val userAnswers: UserAnswers = ua
   "h1" must {
@@ -51,6 +61,24 @@ class TaskListHelperSpec extends SpecBase with MustMatchers with MockitoSugar {
       val expectedBeforeYouStartSection = TaskListEntitySection(None, expectedBeforeYouStartSpoke, beforeYouStartHeader)
 
       helper.beforeYouStartSection mustBe expectedBeforeYouStartSection
+    }
+  }
+
+  "aboutSection " must {
+    "return correct the correct entity section " in {
+      when(mockSpokeCreationService.membershipDetailsSpoke(any(), any())(any())).thenReturn(Seq(expectedMembershipDetailsSpoke))
+      val expectedAboutSection = TaskListEntitySection(None, Seq(expectedMembershipDetailsSpoke), aboutHeader)
+
+      helper.aboutSection mustBe expectedAboutSection
+    }
+  }
+
+  "declarationSection " must {
+    "return correct the correct entity section " in {
+      when(mockSpokeCreationService.declarationSpoke(any())).thenReturn(Seq(expectedDeclarationSpoke))
+      val expectedDeclarationSection = TaskListEntitySection(None, Seq(expectedDeclarationSpoke), declarationHeader, declarationP1: _*)
+
+      helper.declarationSection(false)(completeUserAnswers, implicitly) mustBe Some(expectedDeclarationSection)
     }
   }
 }
--- test/utils/Data.scala
+++ test/utils/Data.scala
@@ -17,12 +17,15 @@
 package utils
 
 import base.SpecBase.fakeRequest
-import identifiers.beforeYouStart.SchemeNameId
-import models.MigrationLock
+import identifiers.aboutMembership.{CurrentMembersId, FutureMembersId}
+import identifiers.beforeYouStart.{EstablishedCountryId, SchemeNameId, SchemeTypeId, WorkingKnowledgeId}
+import models.{Members, MigrationLock, SchemeType}
 import models.requests.DataRequest
+import play.api.libs.json.Format.GenericFormat
 import play.api.libs.json.Json
 import play.api.mvc.AnyContent
 import uk.gov.hmrc.domain.PsaId
+import utils.Data.ua.writes
 
 object Data {
 
@@ -33,6 +36,13 @@ object Data {
   val migrationLock: MigrationLock = MigrationLock(pstr, credId, psaId)
   val schemeName: String = "Test scheme name"
   val ua: UserAnswers = UserAnswers(Json.obj(SchemeNameId.toString -> Data.schemeName))
+  val completeUserAnswers: UserAnswers = UserAnswers().set(SchemeNameId, schemeName).flatMap(
+    _.set(SchemeTypeId, SchemeType.BodyCorporate).flatMap(
+    _.set(EstablishedCountryId, "GB").flatMap(
+    _.set(WorkingKnowledgeId, true).flatMap(
+    _.set(CurrentMembersId, Members.None).flatMap(
+    _.set(FutureMembersId, Members.One)
+    ))))).get
 
   implicit val request: DataRequest[AnyContent] =
     DataRequest(
