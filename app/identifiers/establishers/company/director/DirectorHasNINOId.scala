087aca18d745647decdda80e83b1aedcdd6790dd
jigneshkumarpatel <86969870+jigneshkumarpatel@users.noreply.github.com>
2021-07-23 09:49:05 +0100

    PODS:5756-Add what you need page for director

---
+++ app/identifiers/establishers/company/director/DirectorHasNINOId.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package identifiers.establishers.company.director
+
+import identifiers.TypedIdentifier
+import identifiers.establishers.EstablishersId
+import identifiers.establishers.individual.details.EstablisherHasNINOId
+import play.api.libs.json.{Format, JsPath, Json}
+import utils.UserAnswers
+
+case class DirectorHasNINOId(establisherIndex: Int, directorIndex: Int) extends TypedIdentifier[Boolean] {
+  override def path: JsPath =
+    EstablishersId(establisherIndex).path \ "director" \ directorIndex \  DirectorHasNINOId.toString
+
+  override def cleanup(
+                        value: Option[Boolean],
+                        userAnswers: UserAnswers
+                      ): UserAnswers =
+    value match {
+      case Some(true) =>
+        userAnswers.remove(DirectorNoNINOReasonId(establisherIndex, directorIndex))
+      case Some(false) =>
+        userAnswers.remove(DirectorNINOId(establisherIndex, directorIndex))
+      case _ =>
+        super.cleanup(value, userAnswers)
+    }
+}
+
+object DirectorHasNINOId {
+  override lazy val toString: String =
+    "hasNino"
+
+  implicit lazy val formats: Format[DirectorHasNINOId] =
+    Json.format[DirectorHasNINOId]
+}
+
+
+
