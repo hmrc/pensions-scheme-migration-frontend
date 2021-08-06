5dbaebe0c54d8b8633d2c271b58dff6206e7fec9
jigneshkumarpatel <86969870+jigneshkumarpatel@users.noreply.github.com>
2021-08-06 15:36:17 +0100

    PODS-5756:Correct scalastyle

--- app/controllers/establishers/company/director/details/DirectorDOBController.scala
+++ app/controllers/establishers/company/director/details/DirectorDOBController.scala
@@ -58,7 +58,7 @@ class DirectorDOBController @Inject()(
               dobId        = DirectorDOBId(establisherIndex, directorIndex),
               personNameId = DirectorNameId(establisherIndex, directorIndex),
               schemeName   = schemeName,
-              entityType   = Messages("messages__director"),
+              entityType   = Messages("messages__director")
             )
         }
     }
