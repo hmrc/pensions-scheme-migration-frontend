7bb66ff940d4c29f6db5fae06b64b537414b8d1f
bhatnagarnikita <87030725+bhatnagarnikita@users.noreply.github.com>
2021-08-18 11:12:01 +0100

    PODS-5399 Partnership Details WIP

---
+++ app/controllers/establishers/partnership/details/WhatYouWillNeedController.scala
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.establishers.partnership.details
+
+import controllers.Retrievals
+import controllers.actions.{AuthAction, DataRequiredAction, DataRetrievalAction}
+import controllers.establishers.partnership.details.HaveUTRController
+import helpers.cya.MandatoryAnswerMissingException
+import identifiers.beforeYouStart.SchemeNameId
+import identifiers.establishers.partnership.PartnershipDetailsId
+import models.{Index, NormalMode}
+import play.api.i18n.{I18nSupport, Messages, MessagesApi}
+import play.api.libs.json.Json
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import renderer.Renderer
+import uk.gov.hmrc.nunjucks.NunjucksSupport
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+
+import javax.inject.Inject
+import scala.concurrent.ExecutionContext
+
+class WhatYouWillNeedController @Inject()(
+                                           override val messagesApi: MessagesApi,
+                                           authenticate: AuthAction,
+                                           getData: DataRetrievalAction,
+                                           requireData: DataRequiredAction,
+                                           val controllerComponents: MessagesControllerComponents,
+                                           val renderer: Renderer
+                                         )(implicit val ec: ExecutionContext)
+  extends FrontendBaseController
+    with I18nSupport
+    with Retrievals
+    with NunjucksSupport {
+
+  def onPageLoad(index: Index): Action[AnyContent] =
+    (authenticate andThen getData andThen requireData).async {
+      implicit request =>
+        PartnershipDetailsId(index).retrieve.right.map {
+          details =>
+            renderer.render(
+              template = "establishers/partnership/details/whatYouWillNeed.njk",
+              ctx = Json.obj(
+                "name"        -> details.partnershipName,
+                "pageTitle" -> Messages("messages__establisherDetails__whatYouWillNeed_title"),
+                "continueUrl" -> HaveUTRController.onPageLoad(index, NormalMode).url,
+                "schemeName"  -> request.userAnswers.get(SchemeNameId).getOrElse(throw MandatoryAnswerMissingException)
+              )
+            ).map(Ok(_))
+        }
+    }
+
+}
