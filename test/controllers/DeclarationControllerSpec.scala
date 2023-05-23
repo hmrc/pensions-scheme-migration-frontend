bc0b959afc214a717fe6208874adc1bf62e48ecc
grantrobson <grant.robson@digital.hmrc.gov.uk>
2023-05-23 07:47:57 +0100

    PODS-8399 Fix unit test

--- test/controllers/DeclarationControllerSpec.scala
+++ test/controllers/DeclarationControllerSpec.scala
@@ -193,9 +193,7 @@ class DeclarationControllerSpec extends ControllerSpecBase with NunjucksSupport 
       val result = route(application, httpPOSTRequest(httpPathPOST, Map("value" -> Seq("false")))).value
 
       status(result) mustEqual SEE_OTHER
-      //controllers.routes.YourActionWasNotProcessedController.onPageLoadScheme)
       redirectLocation(result) mustBe Some(controllers.routes.YourActionWasNotProcessedController.onPageLoadScheme.url)
-      //redirectLocation(result) mustBe Some(controllers.routes.TaskListController.onPageLoad.url)
     }
 
     "directs to correct page if 422 response is returned" in {
