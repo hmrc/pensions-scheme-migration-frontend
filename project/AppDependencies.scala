dac2dc7b5362bc802af5776bdb845fa06f821bc0
LauraToddCodes <74312582+LauraToddCodes@users.noreply.github.com>
2024-05-01 12:51:47 +0100

    PODS-9317 - Upgrade dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt._
 
 object AppDependencies {
 
-  private val hmrcBootstrapVersion = "8.4.0"
+  private val hmrcBootstrapVersion = "8.5.0"
 
   private val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"                   %% "bootstrap-frontend-play-30"             % hmrcBootstrapVersion,
-    "uk.gov.hmrc"                   %% "play-nunjucks-viewmodel-play-30"        % "1.0.0",
+    "uk.gov.hmrc"                   %% "play-nunjucks-viewmodel-play-30"        % "1.1.0",
     "org.webjars.npm"               %  "govuk-frontend"                         % "4.7.0",
     "org.webjars.npm"               %  "hmrc-frontend"                          % "1.35.2",
     "uk.gov.hmrc"                   %% "play-conditional-form-mapping-play-30"  % "2.0.0",
@@ -20,10 +20,10 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"   % hmrcBootstrapVersion  % Test,
-    "org.mockito"             %% "mockito-scala"            % "1.17.30"              % Test,
+    "org.mockito"             %% "mockito-scala"            % "1.17.31"              % Test,
     "org.jsoup"               % "jsoup"                     % "1.17.2"              % Test,
-    "com.vladsch.flexmark"    % "flexmark-all"              % "0.64.6"              % "test, it",
-    "org.scalatestplus"       %% "scalacheck-1-17"          % "3.2.17.0"            % Test,
+    "com.vladsch.flexmark"    % "flexmark-all"              % "0.64.8"              % "test, it",
+    "org.scalatestplus"       %% "scalacheck-1-17"          % "3.2.18.0"            % Test,
     "org.pegdown"             % "pegdown"                   % "1.6.0"               % Test
   )
 
