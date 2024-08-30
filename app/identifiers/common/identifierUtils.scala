88c6e55c214a4b99fd392e6a31d6c5cd59957ffd
PavelVjalicin <pavelvjalicin@users.noreply.github.com>
2024-08-30 10:44:53 +0100

    PODS-9735: Refactor

---
+++ app/identifiers/common/identifierUtils.scala
@@ -0,0 +1,12 @@
+package identifiers.common
+
+import models.Index
+import models.entities.{EntityType, PensionManagementType}
+
+object utils {
+  def getEntityTypeId(index: Index,
+                      pensionManagementType: PensionManagementType,
+                      entityType: EntityType) = {
+    
+  }
+}
