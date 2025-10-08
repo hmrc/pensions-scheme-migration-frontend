68491e0545954d03e95f5e3396a024c7bc503c99
AndrewBrown22 <45789050+andrewbrown22@users.noreply.github.com>
2025-10-08 09:31:55 +0100

    DDCNL-11154 PR comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,7 @@ object AppDependencies {
     "com.google.inject.extensions"  %  "guice-multibindings"                    % "4.2.3",
     "uk.gov.hmrc"                   %% "domain-play-30"                         % "12.1.0",
     "com.fasterxml.jackson.module"  %% "jackson-module-scala"                   % "2.20.0",
-    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30"             % "12.10.0"
+    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30"             % "12.17.0"
   )
 
 
