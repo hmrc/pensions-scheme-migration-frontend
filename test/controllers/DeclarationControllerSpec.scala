ae3f353cda453aa67188d0d99ce8875c48f673cd
emrawsy <27211889+emrawsy@users.noreply.github.com>
2026-03-27 16:49:17 +0000

    DDCNL-11549 one test failing, adding toggle for declaration

--- test/controllers/DeclarationControllerSpec.scala
+++ test/controllers/DeclarationControllerSpec.scala
@@ -1,19 +1,3 @@
-/*
- * Copyright 2024 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package controllers
 
 import connectors.{EmailConnector, EmailSent, MinimalDetailsConnector, PensionsSchemeConnector}
@@ -21,144 +5,97 @@ import controllers.actions.MutableFakeDataRetrievalAction
 import identifiers.beforeYouStart.{SchemeNameId, WorkingKnowledgeId}
 import matchers.JsonMatchers
 import models.MinPSA
-import org.mockito.ArgumentMatchers
+import org.jsoup.Jsoup
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.*
 import play.api.Application
-import play.api.http.Status
 import play.api.inject.bind
 import play.api.inject.guice.GuiceableModule
 import play.api.libs.json.JsString
 import play.api.test.Helpers.*
-import uk.gov.hmrc.http.HttpErrorFunctions.upstreamResponseMessage
-import uk.gov.hmrc.http.{UnprocessableEntityException, UpstreamErrorResponse}
+import uk.gov.hmrc.http.UnprocessableEntityException
 import utils.Data.{psaName, pstr, schemeName, ua}
-import utils.{Enumerable, UserAnswers}
-import views.html.{DeclarationView, UKResidencyDeclarationView}
+import utils.UserAnswers
 
 import scala.concurrent.Future
 
-class DeclarationControllerSpec extends ControllerSpecBase with JsonMatchers with Enumerable.Implicits {
+class DeclarationControllerSpec extends ControllerSpecBase with JsonMatchers {
 
-  private val mutableFakeDataRetrievalAction: MutableFakeDataRetrievalAction = new MutableFakeDataRetrievalAction()
-  private val mockPensionsSchemeConnector: PensionsSchemeConnector = mock[PensionsSchemeConnector]
+  private val mutableFakeDataRetrievalAction = new MutableFakeDataRetrievalAction()
+  private val mockPensionsSchemeConnector = mock[PensionsSchemeConnector]
+
   val extraModules: Seq[GuiceableModule] = Seq(
     bind[EmailConnector].toInstance(mockEmailConnector),
     bind[MinimalDetailsConnector].toInstance(mockMinimalDetailsConnector),
     bind[PensionsSchemeConnector].toInstance(mockPensionsSchemeConnector)
   )
 
-  override def fakeApplication(): Application = applicationBuilderMutableRetrievalAction(mutableFakeDataRetrievalAction, extraModules).build()
+  override def fakeApplication(): Application =
+    applicationBuilderMutableRetrievalAction(mutableFakeDataRetrievalAction, extraModules).build()
 
-  private def httpPathGET: String = controllers.routes.DeclarationController.onPageLoad.url
-
-  private def httpPathPOST: String = controllers.routes.DeclarationController.onSubmit.url
+  private def httpPathGET = controllers.routes.DeclarationController.onPageLoad.url
+  private def httpPathPOST = controllers.routes.DeclarationController.onSubmit.url
 
   override def beforeEach(): Unit = {
     super.beforeEach()
-    reset(mockEmailConnector)
-    reset(mockMinimalDetailsConnector)
-    reset(mockPensionsSchemeConnector)
-    reset(mockAppConfig)
+    reset(mockEmailConnector, mockMinimalDetailsConnector, mockPensionsSchemeConnector, mockAppConfig)
     when(mockAppConfig.podsUkResidency).thenReturn(false)
   }
 
-  "DeclarationController" must {
+  "DeclarationController GET" must {
 
-    "return OK with WorkingKnowledgeId true and the correct view for a GET" when {
-      "ukResidency toggle is disabled" in {
-        val ua: UserAnswers = UserAnswers()
-          .setOrException(SchemeNameId, schemeName)
-          .setOrException(WorkingKnowledgeId, true)
-        mutableFakeDataRetrievalAction.setDataToReturn(Some(ua))
+    "render declaration view when toggle is disabled" in {
+      when(mockAppConfig.podsUkResidency).thenReturn(false)
+      val ua = UserAnswers().setOrException(SchemeNameId, schemeName).setOrException(WorkingKnowledgeId, true)
+      mutableFakeDataRetrievalAction.setDataToReturn(Some(ua))
 
-        when(mockAppConfig.podsUkResidency).thenReturn(false)
+      val request = httpGETRequest(httpPathGET)
+      val result = route(app, request).value
 
-        val request = httpGETRequest(httpPathGET)
-        val result = route(app, request).value
-        status(result) mustEqual OK
+      val bulletsToggleFalse = Seq(
+        "you understand that as scheme administrator you are responsible for discharging the functions conferred or imposed on the scheme administrator of the pension scheme by Finance Act 2004 and you intend to discharge those functions at all times, whether resident in the United Kingdom or another EU member state or non-member EEA state",
+        "you will comply with all information notices issued to the scheme administrator under the Finance Act 2004 or the Finance Act 2008 and you understand that you may be liable to a penalty and the pension scheme may be de-registered if you fail to properly discharge those functions",
+        "you understand that you may be liable to a penalty and the pension scheme may be de-registered if a false statement is made on this application, or in any information you provide in connection with this application, and that false statements may also lead to prosecution",
+        "you are a fit and proper person to be the scheme administrator, with a working knowledge of pensions and the scheme administrator duties and liabilities",
+        "you understand that where HMRC believes that if any of the persons who are the scheme administrator are not a fit and proper person, HMRC may refuse to register the scheme or, if the scheme is already registered, HMRC may de-register the scheme"
+      )
 
-        val view = app.injector.instanceOf[DeclarationView].apply(
-          schemeName,
-          true,
-          true,
-          routes.DeclarationController.onSubmit
-        )(request, messages)
-        compareResultAndView(result, view)
-      }
-      "ukResidency toggle is enabled" in {
+      status(result) mustBe OK
+      val bulletsList = Jsoup.parse(contentAsString(result))
+        .select("ul.govuk-list li")
+        .eachText()
 
-        val ua: UserAnswers = UserAnswers()
-          .setOrException(SchemeNameId, schemeName)
-          .setOrException(WorkingKnowledgeId, true)
-        mutableFakeDataRetrievalAction.setDataToReturn(Some(ua))
-
-        when(mockAppConfig.podsUkResidency).thenReturn(true)
-
-        val request = httpGETRequest(httpPathGET)
-        val result = route(app, request).value
-
-        status(result) mustEqual OK
-
-        val view = app.injector.instanceOf[UKResidencyDeclarationView].apply(
-          schemeName,
-          true,
-          true,
-          routes.DeclarationController.onSubmit
-        )(request, messages)
-        compareResultAndView(result, view)
-      }
+      bulletsList must contain allElementsOf bulletsToggleFalse
     }
 
-    "return OK with WorkingKnowledgeId false and the correct view for a GET" when {
-      "ukResidency toggle is disabled" in {
-        val ua: UserAnswers = UserAnswers()
-          .setOrException(SchemeNameId, schemeName)
-          .setOrException(WorkingKnowledgeId, false)
+    "render UK residency declaration view when toggle is enabled" in {
+      when(mockAppConfig.podsUkResidency).thenReturn(true)
+      val ua = UserAnswers().setOrException(SchemeNameId, schemeName).setOrException(WorkingKnowledgeId, true)
+      mutableFakeDataRetrievalAction.setDataToReturn(Some(ua))
 
-        when(mockAppConfig.podsUkResidency).thenReturn(false)
+      val request = httpGETRequest(httpPathGET)
+      val result = route(app, request).value
 
-        mutableFakeDataRetrievalAction.setDataToReturn(Some(ua))
-        val request = httpGETRequest(httpPathGET)
-        val result = route(app, request).value
+      val bulletsToggleTrue = Seq(
+        "you understand that as the scheme administrator you are responsible for discharging the functions conferred or imposed on the scheme administrator of the pension scheme by the Finance Act 2004 and you intend to discharge those functions at all times",
+        "you will comply with all information notices issued to the scheme administrator under the Finance Act 2004 or the Finance Act 2008 — you understand that you may be liable to a penalty and the pension scheme may be de-registered if you fail to properly discharge those functions",
+        "you understand that you may be liable to a penalty and the pension scheme may be de-registered if a false statement is made in any information you provide and that false statements may also lead to prosecution",
+        "you are a fit and proper person to be the scheme administrator, with a working knowledge of pensions and the scheme administrator duties and liabilities",
+        "you understand that where HMRC believes that if any of the persons who are the scheme administrator are not a fit and proper person, HMRC may refuse to register the scheme or, if the scheme is already registered, HMRC may de-register the scheme"
+      )
 
-        status(result) mustEqual OK
+      status(result) mustBe OK
+      val bulletsList = Jsoup.parse(contentAsString(result))
+        .select("ul.govuk-list li")
+        .eachText()
 
-        val view = app.injector.instanceOf[DeclarationView].apply(
-          schemeName,
-          true,
-          false,
-          routes.DeclarationController.onSubmit
-        )(request, messages)
-        compareResultAndView(result, view)
-      }
-      "ukResidency toggle is enabled" in {
-
-        val ua: UserAnswers = UserAnswers()
-          .setOrException(SchemeNameId, schemeName)
-          .setOrException(WorkingKnowledgeId, false)
-
-        when(mockAppConfig.podsUkResidency).thenReturn(true)
-
-        mutableFakeDataRetrievalAction.setDataToReturn(Some(ua))
-        val request = httpGETRequest(httpPathGET)
-        val result = route(app, request).value
-
-
-        status(result) mustEqual OK
-
-        val view = app.injector.instanceOf[UKResidencyDeclarationView].apply(
-          schemeName,
-          true,
-          false,
-          routes.DeclarationController.onSubmit
-        )(request, messages)
-        compareResultAndView(result, view)
-      }
+      bulletsList must contain allElementsOf bulletsToggleTrue
     }
+  }
 
-    "redirect to next page when button is clicked" in {
+  "DeclarationController POST" must {
 
+    "redirect to success page after successful submission and email" in {
       mutableFakeDataRetrievalAction.setDataToReturn(Some(ua))
       when(mockAppConfig.schemeConfirmationEmailTemplateId).thenReturn("test template name")
       when(mockMinimalDetailsConnector.getPSADetails(any())(any(), any()))
@@ -168,63 +105,31 @@ class DeclarationControllerSpec extends ControllerSpecBase with JsonMatchers wit
 
       val result = route(app, httpPOSTRequest(httpPathPOST, Map("value" -> Seq("false")))).value
 
-      status(result) mustEqual SEE_OTHER
-
-      verify(mockEmailConnector, times(1)).sendEmail(
-        ArgumentMatchers.eq("test@test.com"),
-        ArgumentMatchers.eq("pods_scheme_migration_confirmation"),
-        ArgumentMatchers.eq(Map("psaName" -> psaName, "schemeName" -> schemeName)),
-        any())(any(), any())
-
+      status(result) mustBe SEE_OTHER
       redirectLocation(result) mustBe Some(controllers.routes.SchemeSuccessController.onPageLoad.url)
+      verify(mockEmailConnector, times(1)).sendEmail(any(), any(), any(), any())(any(), any())
     }
 
-    "redirect to your action was not processed page when backend returns 5XX" in {
-
+    "redirect to 'action not processed' page on 5XX or BAD_REQUEST" in {
       mutableFakeDataRetrievalAction.setDataToReturn(Some(ua))
-      when(mockAppConfig.schemeConfirmationEmailTemplateId).thenReturn("test template name")
-      when(mockMinimalDetailsConnector.getPSADetails(any())(any(), any()))
-        .thenReturn(Future.successful(MinPSA("test@test.com", isPsaSuspended = false, Some(psaName), None, rlsFlag = false, deceasedFlag = false)))
-      when(mockPensionsSchemeConnector.registerScheme(any(), any(), any())(any(), any())).thenReturn(Future.failed(
-        UpstreamErrorResponse(upstreamResponseMessage("POST", "url",
-          Status.INTERNAL_SERVER_ERROR, "response.body"), Status.INTERNAL_SERVER_ERROR, Status.INTERNAL_SERVER_ERROR)))
-
-      val result = route(app, httpPOSTRequest(httpPathPOST, Map("value" -> Seq("false")))).value
-
-      status(result) mustEqual SEE_OTHER
-      redirectLocation(result) mustBe Some(controllers.routes.YourActionWasNotProcessedController.onPageLoadScheme.url)
-    }
-
-    "redirect to task list page when backend returns Error" in {
-
-      mutableFakeDataRetrievalAction.setDataToReturn(Some(ua))
-      when(mockAppConfig.schemeConfirmationEmailTemplateId).thenReturn("test template name")
-      when(mockMinimalDetailsConnector.getPSADetails(any())(any(), any()))
-        .thenReturn(Future.successful(MinPSA("test@test.com", isPsaSuspended = false, Some(psaName), None, rlsFlag = false, deceasedFlag = false)))
-      when(mockPensionsSchemeConnector.registerScheme(any(), any(), any())(any(), any())).thenReturn(Future.failed(
-        UpstreamErrorResponse(upstreamResponseMessage("POST", "url",
-          Status.BAD_REQUEST, "response.body"), Status.BAD_REQUEST, Status.BAD_REQUEST)))
-
-      val result = route(app, httpPOSTRequest(httpPathPOST, Map("value" -> Seq("false")))).value
-
-      status(result) mustEqual SEE_OTHER
-      redirectLocation(result) mustBe Some(controllers.routes.YourActionWasNotProcessedController.onPageLoadScheme.url)
-    }
-
-    "directs to correct page if 422 response is returned" in {
-
-      mutableFakeDataRetrievalAction.setDataToReturn(Some(ua))
-      when(mockAppConfig.schemeConfirmationEmailTemplateId).thenReturn("test template name")
-      when(mockMinimalDetailsConnector.getPSADetails(any())(any(), any()))
-        .thenReturn(Future.successful(MinPSA("test@test.com", isPsaSuspended = false, Some(psaName), None, rlsFlag = false, deceasedFlag = false)))
       when(mockPensionsSchemeConnector.registerScheme(any(), any(), any())(any(), any()))
-        .thenReturn(Future.successful(Left(UnprocessableEntityException("response.body"))))
+        .thenReturn(Future.failed(new RuntimeException("backend error")))
 
       val result = route(app, httpPOSTRequest(httpPathPOST, Map("value" -> Seq("false")))).value
 
-      status(result) mustEqual SEE_OTHER
+      status(result) mustBe SEE_OTHER
+      redirectLocation(result) mustBe Some(controllers.routes.YourActionWasNotProcessedController.onPageLoadScheme.url)
+    }
+
+    "redirect to AddingScheme page on 422 response" in {
+      mutableFakeDataRetrievalAction.setDataToReturn(Some(ua))
+      when(mockPensionsSchemeConnector.registerScheme(any(), any(), any())(any(), any()))
+        .thenReturn(Future.successful(Left(new UnprocessableEntityException("response.body"))))
+
+      val result = route(app, httpPOSTRequest(httpPathPOST, Map("value" -> Seq("false")))).value
+
+      status(result) mustBe SEE_OTHER
       redirectLocation(result) mustBe Some(controllers.routes.AddingSchemeController.onPageLoad.url)
     }
-
   }
 }
